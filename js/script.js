3998ffcca445c24f53f790d5303456834538003e
Edgardo Ezequiel Silveira <103297004+ezesilveira@users.noreply.github.com>
2022-05-29 00:58:26 -0300

    1

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var VariableTextoIngresado = ""
+    var VariableTextoEncriptado = ""
+    var VariableTextoDesencriptado = ""
+
+    function encriptar(){
+        VariableTextoIngresado = document.getElementById("textoIngresado").value;
+        document.getElementById("resultado").style.display = "none";
+        
+        
+    }
+    function desencriptar(){
+        VariableTextoIngresado = document.getElementById("textoIngresado").value;
+        document.getElementById("resultado").style.display = "none";
+        
+    }  
