688b668cf93abbf1168783c1ec984f139eedf38f
Edgardo Ezequiel Silveira <103297004+ezesilveira@users.noreply.github.com>
2022-06-05 22:03:19 -0300

    final

--- js/script.js
+++ js/script.js
@@ -7,8 +7,8 @@ var chars = {'a':'ai','e':'enter','i':'imes','o':'ober','u':'ufat'};
 function encriptar(){
     var e = document.getElementById("textoIngresado").value;
     var hh = e.replace(/[aeiou]/g, m => chars[m]);
-    
     document.getElementById("textoResultado").value = hh;
+    display();
 }
 function desencriptar(){
     let replacementMap = {"ai":"a","enter": "e","imes": "i","ober": "o","ufat": "u"}
@@ -17,24 +17,16 @@ function desencriptar(){
     for (key in replacementMap) {
 		sentence = sentence.replaceAll(key, replacementMap[key])
     }
-
     document.getElementById("textoResultado").value = sentence;
+    display();
 }
 
-/*var campoResultado = document.querySelector("#textoIngresado");
-campoResultado.addEventListener("input", function(){
-    var textoIngresado = document.querySelectorAll(".textoIngresado");
-
-    if(textoIngresado.length > 0){
-       
-        document.getElementById("resultado").style.display = "none";
-        document.getElementById("textoResultado").style.display = "inline-block";   
-        
-    }else{
-        document.getElementById("resultado").style.display = "inline-block";
-        document.getElementById("textoResultado").style.display = "none";  
-        } 
-    }  
-
-);
-*/
+function display(){
+    document.getElementById("imagenesResultado").style.display = "none";
+    document.getElementById("resultadoFinal").style.display = "inline-block";  
+}
+function copiarPortapapeles() {
+    let copyText = document.querySelector("#textoResultado");
+    copyText.select();
+    document.execCommand("copy");
+}
